f2179ab60077725d06dfb8813113b8713dacbe2a
Ebuk-a <cakwiwuuzoma@gmail.com>
2023-07-21 17:30:54 +0100

    update driver points dbt model

--- dbt/f1_race/models/driver_points.sql
+++ dbt/f1_race/models/driver_points.sql
@@ -8,13 +8,13 @@ with races as (
 final as (
 
     select "driver_id",  
-            sum("points") as "total_pointsss" 
+            sum("points") as "total_points" 
             
     from races
 
     group by "driver_id"
 
-    order by "total_pointsss" desc 
+    order by "total_points" desc 
 
 )
 
